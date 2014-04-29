b3b24224b7bc69adbcb6b9943607f4ad6f89a9ef
Mike Green <nekbet@gmail.com>
2014-04-29 09:41:38 -0400

    database connection example file.
    rename to dbCreds.py for use

---
+++ dbCreds_example.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+# this is dbCreds
+
+import MySQLdb as mdb
+
+# Example
+# con = mdb.connect('mysql.yourserver.com', 'username', 'password', 'database_name');
+con = mdb.connect('mysql.yourserver.com', 'username', 'password', 'database_name');
