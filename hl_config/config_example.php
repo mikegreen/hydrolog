9bd4d2265630cac32a58044b151118f8789835e1
Mike Green <nekbet@gmail.com>
2014-04-29 09:46:47 -0400

    hl_config initial commit

---
+++ hl_config/config_example.php
@@ -0,0 +1,10 @@
+// rename this to config.php and fill in your details below
+
+<?php
+$dbms = 'mysqli';
+$dbhost = 'serverAddress.com';
+$dbport = '3306';
+$dbname = 'database_name';
+$dbuser = 'username';
+$dbpasswd = 'password';
+?>
