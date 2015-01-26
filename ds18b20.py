3c71f1836aa490dc146ea75929ebbbc904d5f9c9
Mike Green <nekbet@gmail.com>
2015-01-26 02:55:03 +0000

    Adafruit_DHT_googledocs.ex.py

--- ds18b20.py
+++ ds18b20.py
@@ -4,8 +4,9 @@ import time
 
 print "ds18b20 start"
 
-os.system('modprobe w1-gpio')
-os.system('modprobe w1-therm')
+# loading these in /etc/modules now
+# os.system('modprobe w1-gpio')
+# os.system('modprobe w1-therm')
 base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
